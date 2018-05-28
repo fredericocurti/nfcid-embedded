dd83732af11ffe9df769295dd54e1e37e2f69de1
vilela69 <frederico.vilelacurti@gmail.com>
2018-05-27 21:46:14 -0300

    app final

--- NDEFred/app/src/main/java/app/nfcbois/ndefred/MainActivity.java
+++ NDEFred/app/src/main/java/app/nfcbois/ndefred/MainActivity.java
@@ -1,23 +1,35 @@
 package app.nfcbois.ndefred;
 
+import android.animation.ArgbEvaluator;
+import android.animation.ObjectAnimator;
+import android.content.Context;
 import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.nfc.NdefMessage;
 import android.nfc.NdefRecord;
 import android.nfc.NfcAdapter;
 import android.nfc.NfcEvent;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.WindowManager;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements NfcAdapter.CreateNdefMessageCallback, NfcAdapter.OnNdefPushCompleteCallback {
     NfcAdapter mNfcAdapter;
     Button mButton;
     EditText mEdit;
-    SharedPreferences mPrefs;
-    SharedPreferences.Editor mEditor;
+    SharedPreferences settings;
+    ConstraintLayout screen;
+    SharedPreferences.Editor editor;
     String id = null;
 
     @Override
@@ -25,14 +37,35 @@ public class MainActivity extends AppCompatActivity implements NfcAdapter.Create
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        settings = getSharedPreferences("UserInfo", 0);
+        editor = settings.edit();
+
+        id = settings.getString("id", "");
         mButton = (Button)findViewById(R.id.button);
         mEdit   = (EditText)findViewById(R.id.form);
+        screen = (ConstraintLayout)findViewById(R.id.layout);
+        screen.requestFocus();
+
+        mEdit.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if (actionId == EditorInfo.IME_ACTION_DONE) {
+                    mButton.performClick();
+                }
+                return false;
+            }
+        });
+
+        // puts retrieved text;
+        mEdit.setText(id);
 
         mButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Log.v("EditText", mEdit.getText().toString());
                 id = mEdit.getText().toString();
+                editor.putString("id", id);
+                editor.apply();
             }
         });
 
@@ -59,6 +92,14 @@ public class MainActivity extends AppCompatActivity implements NfcAdapter.Create
 
     @Override
     public void onNdefPushComplete(NfcEvent event) {
+        Log.d("HEY", "SUCCESSS");
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                Toast.makeText(MainActivity.this, "Bem vindo!",
+                        Toast.LENGTH_LONG).show();
+            }
+        });
 
     }
 }
